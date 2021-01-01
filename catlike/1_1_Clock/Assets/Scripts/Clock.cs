85c9923749310b5d7a4f3976272e970ea1d6c348
inexts1996 <inexts0618@gmail.com>
2021-01-02 01:46:41 +0800

    finish clock

---
+++ catlike/1_1_Clock/Assets/Scripts/Clock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    [SerializeField] private Transform hoursPivot, minutesPivot, secondsPivot;
+
+    private const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
+
+    private TimeSpan time;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        time = DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation = Quaternion.Euler(0, 0, hoursToDegrees * (float)time.TotalHours);
+        minutesPivot.localRotation = Quaternion.Euler(0, 0, minutesToDegrees * (float)time.TotalMinutes);
+        secondsPivot.localRotation = Quaternion.Euler(0, 0, secondsToDegrees * (float)time.TotalSeconds);
+    }
+}
