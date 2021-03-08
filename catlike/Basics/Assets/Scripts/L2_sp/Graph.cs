80f526ceb64b7b1df1830adc2e171775da0af4ca
inexts1996 <inexts0618@gmail.com>
2021-03-08 23:25:19 +0800

    learn building a graph

---
+++ catlike/Basics/Assets/Scripts/L2_sp/Graph.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+	[SerializeField]
+	Transform pointPrefab = default;
+    // Start is called before the first frame update
+	
+	void Awake()
+	{
+		Transform point;
+		int i = 0;
+		while(i < 10)
+		{
+			point = Instantiate(pointPrefab);
+			point.localPosition = Vector3.right * i;
+			++i;
+		}
+	}
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
