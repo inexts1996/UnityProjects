0ccf74723d581442cdfc8bc2bce5d1251b44bbd1
inexts1996 <inexts0618@gmail.com>
2021-03-10 07:28:20 +0800

    finish building a graph

--- catlike/Basics/Assets/Scripts/L2_sp/Graph.cs
+++ catlike/Basics/Assets/Scripts/L2_sp/Graph.cs
@@ -6,19 +6,31 @@ public class Graph : MonoBehaviour
 {
 	[SerializeField]
 	Transform pointPrefab = default;
+	[SerializeField, Range(10, 100)]
+	int resolution = 10;
     // Start is called before the first frame update
 	
+		Transform[] points;
 	void Awake()
 	{
+		float step = 2f / resolution;
+		var scale = Vector3.one * step;
+		var position = Vector3.zero;
+		points = new Transform[resolution];
 		Transform point;
-		int i = 0;
-		while(i < 10)
+
+		for(int i = 0; i < resolution; ++i)
 		{
 			point = Instantiate(pointPrefab);
-			point.localPosition = Vector3.right * i;
-			++i;
+			position.x=  (i + 0.5f) * step - 1f;
+			point.localPosition = position; 
+			point.localScale =scale;
+			point.SetParent(transform, false);
+
+			points[i] = point;
 		}
 	}
+
     void Start()
     {
         
@@ -27,6 +39,12 @@ public class Graph : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+	    for (int i = 0; i <resolution; i++)
+	    {
+		    Transform point = points[i];
+		    Vector3 position = point.localPosition;
+		    position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+		    point.localPosition = position;
+	    } 
     }
 }
---
+++ catlike/Basics/Assets/Scripts/Shaders/PointSurface.shader
@@ -0,0 +1,33 @@
+﻿Shader "Graph/PointSurface"
+{
+    Properties
+    {
+        _Smoothness("Smoothness", Range(0, 1)) = 0.5    
+    }
+    
+    SubShader
+    {
+
+        CGPROGRAM
+        // Physically based Standard lighting model, and enable shadows on all light types
+        #pragma surface surf Standard fullforwardshadows
+
+        // Use shader model 3.0 target, to get nicer looking lighting
+        #pragma target 3.0
+
+
+        struct Input
+        {
+            float3 worldPos;
+        };
+
+        float _Smoothness;
+        void surf (Input input, inout SurfaceOutputStandard surface)
+        {
+            surface.Albedo.rg = input.worldPos.xy * 0.5 + 0.5;
+            surface.Smoothness = _Smoothness;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
